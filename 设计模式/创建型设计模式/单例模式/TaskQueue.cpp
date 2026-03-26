365bfc7a61c3ea7011d57bbd4d8f44f95236705f
zymJDFH <2994059821@qq.com>
2026-03-26 21:58:03 +0800

    3.26

--- 设计模式/创建型设计模式/单例模式/TaskQueue.cpp
+++ 设计模式/创建型设计模式/单例模式/TaskQueue.cpp
@@ -5,6 +5,7 @@
 #include <mutex>
 #include <queue>
 #include <thread>
+#include <vector>
 
 using namespace std;
 
@@ -13,32 +14,33 @@ private:
     queue<function<void()>> tasks_;
     mutex mtx_;
     condition_variable cv_;
-    bool running =false;
-    vector<thread>workers_;
+    bool running = false;
+    vector<thread> workers_;
+    mutex cout_mtx;
     TaskQueue() = default;
-    ~TaskQueue() { 
-        stop(); 
-        for(auto &t:workers_){
-            t.join();
+    ~TaskQueue() {
+        stop();
+        for (auto& w : workers_) {
+            if (w.joinable()) w.join();
         }
     }
     TaskQueue(const TaskQueue&) = delete;
     TaskQueue& operator=(const TaskQueue&) = delete;
-    void workerLoop(){
-        while(true){
-            function<void()>task;
-            {
-                unique_lock<mutex>lock(mtx_);
-                cv_.wait(lock,[this]{return !tasks_.empty()||!running;});
-                if(tasks_.empty()&&!running)return ;
 
-                task=move(tasks_.front());
+    void workerLoop() {
+        while (true) {
+            function<void()> task;
+            {
+                unique_lock<mutex> lock(mtx_);
+                cv_.wait(lock, [this] { return !tasks_.empty() || !running; });
+                if (tasks_.empty() && !running) return;
+                if (tasks_.empty()) continue;
+                task = move(tasks_.front());
                 tasks_.pop();
             }
-            if(task)task();
+            if (task) task();
         }
     }
-    
 
 public:
     static TaskQueue* getInstance() {
@@ -46,39 +48,52 @@ public:
         return &q;
     }
 
-    void start() {
-        unique_lock<mutex>lock(mtx_);
-        if(running)return ;
-        running=true;
-        workers_.emplace_back([this]{this->workerLoop();});
+    void start(size_t threadCount = 4) {
+        unique_lock<mutex> lock(mtx_);
+        if (running) return;
+        running = true;
+        for (size_t i = 0; i < threadCount; ++i) {
+            workers_.emplace_back([this] { this->workerLoop(); });
+        }
     }
 
     bool push(function<void()> task) {
-       {
-            unique_lock<mutex>lock(mtx_);
-            if(!running)return false;
+        {
+            unique_lock<mutex> lock(mtx_);
+            if (!running) return false;
             tasks_.push(move(task));
-       }
-       cv_.notify_one();
-       return true;
+        }
+        cv_.notify_one();
+        return true;
     }
 
     void stop() {
         {
-            unique_lock<mutex>lock(mtx_);   
-            if(!running)return ;
-            running =false;
+            unique_lock<mutex> lock(mtx_);
+            if (!running) return;
+            running = false;
         }
         cv_.notify_all();
+    }
+    template<typename... Args>
+    void print(Args&&...args){
+        lock_guard<mutex> lock(cout_mtx);  
+        (cout << ... << forward<Args>(args)) << endl; 
     }
 };
 
 int main() {
     TaskQueue* q = TaskQueue::getInstance();
-    q->start();
-    q->push([]{cout<<"task 1 "<<endl;});
-    q->push([]{cout<<"task 2 "<<endl;});
-    this_thread::sleep_for(chrono::milliseconds(100));
+    q->start(4); 
+    
+    for (int i = 0; i < 10; ++i) {
+       q->push([i, q] {
+            q->print("task ", i, " thread_id=", this_thread::get_id());
+            this_thread::sleep_for(chrono::milliseconds(50));
+        });
+    }
+
+    this_thread::sleep_for(chrono::milliseconds(300));
     q->stop();
     return 0;
 }
